3bcf2778a3a33174d2ae789544fe6b22dc4508d0
William Warner Burke <williamburke121@gmail.com>
2022-01-05 20:23:00 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.`
+}
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional (Gracie, language = "JavaScript"){
+    return `Hi, my name is ${Gracie} and I am learning to program in ${language}.`
+}
